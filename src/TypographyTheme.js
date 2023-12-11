a4c96a95f8f4479b8e3f593e9484ce329d1d38fe
lderequesens <leonardo.sobarzo@protonmail.com>
2023-12-11 14:51:24 -0300

    Add vecna font

---
+++ src/TypographyTheme.js
@@ -0,0 +1,34 @@
+import vecna from "./Vecna-oppx.ttf"
+import { createTheme } from "@mui/material";
+
+export const typoTheme = createTheme({
+	typography: {
+		fontFamily: 'Vecna',
+		allVariants: 'normal',
+		fontSize: 20,
+	},
+	components: {
+		MuiCssBaseline: {
+		styleOverrides: `
+		@font-face {
+		  font-family: 'Vecna';
+		  font-style: normal;
+		  font-display: swap;
+		  src: url(${vecna}) format('truetype');
+		  unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+		}
+		`,
+		},
+	},
+	palette: {
+        atWill:{
+            main: '#649769'
+        },
+        encounter:{
+            main: "#981332"
+        },
+        daily:{
+            main: "#4c4c4e"
+        },
+	}
+});
--- src/Viewer.jsx
+++ src/Viewer.jsx
@@ -1,16 +1,19 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, ThemeProvider, Typography, CssBaseline } from "@mui/material";
 import { PropertyListViewer } from "./PropertyListViewer";
+import { typoTheme } from "./TypographyTheme";
 
 export const Viewer = ({barColor, name, level, powerType, keywords, powerAction, range, properties, description}) => {
 	return (
 		<>
-			<Grid item xs={12} sm={10} md={5} sx={{border:1, ml:{sm:0,md:4}, mb:{xs:20,sm:20,md:0}, minHeight:{sm:'500px',md:0}, height:'500px'}} >
+			<ThemeProvider theme={typoTheme}>
+			<CssBaseline/>
+			<Grid item xs={12} sm={10} md={5} sx={{border:1, ml:{sm:0,md:4}, mb:{xs:20,sm:20,md:0}, minHeight:{sm:'500px',md:0}, height:(300 + properties.length * 40) + 'px'}} >
 				<Grid container sx={{height:'50px', backgroundColor:barColor, color:'white'}} justifyContent={'space-between'} alignItems={'center'}>
 					<Grid item ml={2}>
 						{/*Todo change icon*/}
 						{'icono'}
 					</Grid>
-					<Typography sx={{fontWeight: 'bold'}}>{name}</Typography>
+					<Typography sx={{fontWeight: 'bold', fontFamily: 'Vecna'}}>{name}</Typography>
 					<Grid item mr={2}>
 						<Typography sx={{fontWeight: 'bold'}}>{level}</Typography>
 					</Grid>
@@ -31,6 +34,7 @@ export const Viewer = ({barColor, name, level, powerType, keywords, powerAction,
 				</Grid>
 
 			</Grid>
+			</ThemeProvider>
 		</>
 	);
 };
